63948301623d3561adcaa061a6813b57c792e400
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2022-01-11 13:30:32 -0300

    Add Heroku needed config

--- index.js
+++ index.js
@@ -177,7 +177,8 @@ const unknownEndpoint = (request, response) => {
 /* Middleware para capturar los endpoints que no son manejados por la app */
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
