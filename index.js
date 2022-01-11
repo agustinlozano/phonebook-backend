7f7c7b74b12c67364815c21619c0b64458348f11
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2022-01-11 11:03:11 -0300

    Add Eslint dependency

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
-/*  Configure morgan para que también muestre los 
+/*  Configure morgan para que también muestre los
  *  datos enviados en las solicitudes HTTP POST:
  *    - No funciona
  */
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :param[name] :param[number]'))
 
-morgan.token('param', (req, res, param) => 
+morgan.token('param', (req, res, param) =>
   res.params[param]
 )
 
@@ -44,7 +44,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/persons', (req, res) => {
-  res.json(personsList);
+  res.json(personsList)
 })
 
 /*  Un GET -> /info devuelve un fragmento HTML  */
@@ -62,7 +62,7 @@ app.get('/info', (req, res) => {
  *
  *  Es importante saber que este metodo es conocido
  *  como un Middleware, es decir una funcion que es
- *  llamada entre el procesamiento de la request y 
+ *  llamada entre el procesamiento de la request y
  *  el envio de la respuesta.
  */
 /* GET -> person */
@@ -71,12 +71,12 @@ app.get('/api/persons/:id', (req, res) => {
   const contact = personsList.find(person =>
     person.id === currentID
   )
-  
+
   console.log('contactID: ', contact)
-  
+
   if (contact) {
     // res.send(`<p>Contact <strong>${contact.name}</strong></p>`)
-    res.json(contact) 
+    res.json(contact)
   } else {
     res.status(404).send('Not faund')
   }
@@ -85,7 +85,7 @@ app.get('/api/persons/:id', (req, res) => {
 /*  Luego de obtener el id filtramos con el metodo
  *  filter el array de contactos devolviendo todos
  *  miembros excepto aquel que contiene dicho id.
- *  
+ *
  *  Finalmente actualizamos el valor de la lista de
  *  contactos.
  */
@@ -95,32 +95,32 @@ app.delete('/api/persons/:id', (req, res) => {
   const newContacs = personsList.filter(person =>
     person.id !== currentID
   )
-  
+
   personsList = newContacs
   res.status(204).end()
 })
 
 /*  Para alterar los datos en el servidor en nuestro
- *  caso tenemos que asegurarnos de generar un id 
+ *  caso tenemos que asegurarnos de generar un id
  *  para el nuevo recurso.
- * 
+ *
  *  Luego, con el metodo body podemos acceder al cuerpo
  *  de la request y asi setear el nuevo contacto en un
  *  nuevo objeto. Finalente modificamos nuestra lista
  *  de contactos concatenando con el metodo concat.
  */
- /* POST -> persons */
+/* POST -> persons */
 app.post('/api/persons', (req, res) => {
-  const id = Math.round(Math.random()*1000)
+  const id = Math.round(Math.random() * 1000)
   const body = req.body
-  
+
   console.log('body: ', body)
   /* body = { id, name, phone } */
-  
+
   const sameName = personsList.find(person =>
-    person.name === body.name 
+    person.name === body.name
   )
-  
+
   /* validaciones */
   if (sameName) {
     res.status(400).json({
@@ -136,10 +136,10 @@ app.post('/api/persons', (req, res) => {
       name: body.name,
       phone: body.number
     }
-    
+
     /* Modificar -actualiza- la iformacion en el servidor */
     const newList = personsList.concat(newContac)
-  
+
     personsList = newList
     res.json(personsList)
   }
