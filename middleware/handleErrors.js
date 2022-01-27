75eb8e583e3d03d650cdd22e6fd4812e90da95f6
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2022-01-27 11:19:26 -0300

    Edit handleErrors: add new log error

--- middleware/handleErrors.js
+++ middleware/handleErrors.js
@@ -1,5 +1,6 @@
 const handleErrors = (error, req, res, next) => {
   if (error.name === 'CastError') {
+    console.log(error.message)
     res.status(400).send({ error: 'id used is malformed' })
   } else {
     res.status(500).end()
