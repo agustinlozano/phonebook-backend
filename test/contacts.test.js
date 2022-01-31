16f570fb8d49bbfa86602997fbec1728a68dab2f
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2022-01-31 10:43:55 -0300

    Add new controller tests with SuperTest

--- test/contacts.test.js
+++ test/contacts.test.js
@@ -1,15 +1,75 @@
 const mongoose = require('mongoose')
-const supertest = require('supertest')
-const app = require('../app')
+
 const server = require('../index')
+const ContactModel = require('../models/Contact')
+const {
+  api,
+  initialContacts,
+  getAllNamesFromContacts
+} = require('./helpers')
 
-const api = supertest(app)
+beforeEach(async () => {
+  await ContactModel.deleteMany({})
 
-test('notes are returned as json', async () => {
+  const contact1 = new ContactModel(initialContacts[0])
+  await contact1.save()
+
+  const contact2 = new ContactModel(initialContacts[1])
+  await contact2.save()
+
+  const contact3 = new ContactModel(initialContacts[2])
+  await contact3.save()
+})
+
+test('contacts are returned as json', async () => {
   await api
     .get('/api/persons')
     .expect(200)
     .expect('Content-Type', /json/)
+})
+
+test('there are three contacts', async () => {
+  const { response } = await getAllNamesFromContacts()
+  expect(response.body).toHaveLength(initialContacts.length)
+})
+
+test('a contact have my name', async () => {
+  const { names } = await getAllNamesFromContacts()
+  expect(names).toContain('Agustin Lozano')
+})
+
+test('a valid conctact can be added', async () => {
+  const newContact = {
+    name: 'Carlos Lozano',
+    phone: '2477 - 677392'
+  }
+
+  await api
+    .post('/api/persons')
+    .send(newContact)
+    .expect(200)
+    .expect('Content-Type', /json/)
+
+  const { names } = await getAllNamesFromContacts()
+
+  expect(names).toContain(newContact.name)
+  expect(names).toHaveLength(initialContacts.length + 1)
+})
+
+test('a invalid contact cannot be added', async () => {
+  const newBadConctact = {
+    name: 'bad user',
+    phone: undefined
+  }
+
+  await api
+    .post('/api/persons')
+    .send(newBadConctact)
+    .expect(400)
+
+  const { names } = await getAllNamesFromContacts()
+
+  expect(names).toHaveLength(initialContacts.length)
 })
 
 afterAll(() => {
---
+++ test/helpers.js
@@ -0,0 +1,31 @@
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+
+const initialContacts = [
+  {
+    name: 'Agustin Lozano',
+    phone: '2477 - 635371'
+  },
+  {
+    name: 'Celeste Tessone',
+    phone: '2477 - 397271'
+  },
+  {
+    name: 'Vicente Lozano',
+    phone: '2477 - 639430'
+  }
+]
+
+const getAllNamesFromContacts = async () => {
+  const response = await api.get('/api/persons')
+  const names = response.body.map(contact => contact.name)
+
+  return { response, names }
+}
+
+module.exports = {
+  api,
+  initialContacts,
+  getAllNamesFromContacts
+}
