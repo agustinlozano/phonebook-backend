4e79f581c93d0e4d01b8ab5301a48eb79f6d8876
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2022-02-07 14:18:50 -0300

    Edit tests: add describe blocks and some changes

--- test/contacts.test.js
+++ test/contacts.test.js
@@ -25,99 +25,122 @@ beforeEach(async () => {
   }
 })
 
-test('contacts are returned as json', async () => {
-  await api
-    .get('/api/persons')
-    .expect(200)
-    .expect('Content-Type', /json/)
+describe('GET /api/persons', () => {
+  test('contacts are returned as json', async () => {
+    await api
+      .get('/api/persons')
+      .expect(200)
+      .expect('Content-Type', /json/)
+  })
 })
 
-test('there are three contacts', async () => {
-  const { response } = await getAllFromContacts()
-  expect(response.body).toHaveLength(initialContacts.length)
+describe('POST /api/persons', () => {
+  test('a valid contact can be added', async () => {
+    const newContact = {
+      name: 'Carlos Lozano',
+      phone: '2477 - 677392'
+    }
+
+    await api
+      .post('/api/persons')
+      .send(newContact)
+      .expect(200)
+      .expect('Content-Type', /json/)
+
+    const { names } = await getAllFromContacts()
+
+    expect(names).toContain('Carlos Lozano')
+    expect(names).toHaveLength(initialContacts.length + 1)
+  })
+
+  test('a invalid contact cannot be added', async () => {
+    const newBadConctact = {
+      name: 'bad user',
+      phone: undefined
+    }
+
+    await api
+      .post('/api/persons')
+      .send(newBadConctact)
+      .expect(400)
+
+    const { response } = await getAllFromContacts()
+    const contacts = response.body
+
+    expect(contacts).toHaveLength(initialContacts.length)
+  })
 })
 
-test('a contact have my name', async () => {
-  const { names } = await getAllFromContacts()
-  expect(names).toContain('Agustin Lozano')
+describe('GET /api/persons/:id', () => {
+  test('a single contact can be viewed', async () => {
+    const { response } = await getAllFromContacts()
+    const contacts = response.body
+    const existingContact = contacts[0]
+
+    await api
+      .get(`/api/persons/${existingContact.id}`)
+      .expect(200)
+      .expect('Content-Type', /json/)
+  })
+
+  test('a nonexisting id return 404 status code', async () => {
+    const invalidID = '61f85179ff08013914dd882d'
+
+    await api
+      .get(`/api/persons/${invalidID}`)
+      .expect(404)
+  })
+
+  test('an invalid id return 400 status code', async () => {
+    const invalidID = '1234'
+
+    await api
+      .get(`/api/persons/${invalidID}`)
+      .expect(400)
+  })
 })
 
-test('contact is returned as json', async () => {
-  const { response } = await getAllFromContacts()
-  const contacts = response.body
-  const firstContact = contacts[0]
+describe('DELETE /api/persons/:id', () => {
+  test('a existing contact can be deleted', async () => {
+    const { response: firstResponse } = await getAllFromContacts()
+    const contacts = firstResponse.body
+    const existingContact = contacts[0]
 
-  console.log(firstContact.id)
-  await api
-    .get(`/api/persons/${firstContact.id}`)
-    .expect(200)
-    .expect('Content-Type', /json/)
+    await api
+      .delete(`/api/persons/${existingContact.id}`)
+      .expect(204)
+
+    const { names, response: secondResponse } = await getAllFromContacts()
+
+    expect(secondResponse.body).toHaveLength(initialContacts.length - 1)
+    expect(names).not.toContain(existingContact.name)
+  })
+
+  test('a contact that not exist cannot be deleted', async () => {
+    const invalidPATH = '/api/persons/1234'
+
+    await api
+      .delete(invalidPATH)
+      .expect(400)
+
+    const { response } = await getAllFromContacts()
+
+    expect(response.body).toHaveLength(initialContacts.length)
+  })
 })
 
-test('a valid contact can be added', async () => {
-  const newContact = {
-    name: 'Carlos Lozano',
-    phone: '2477 - 677392'
-  }
+// describe('PUT /api/persons/:id', () => {})
 
-  await api
-    .post('/api/persons')
-    .send(newContact)
-    .expect(200)
-    .expect('Content-Type', /json/)
+describe('test the initial contacts list', () => {
+  test('there are three contacts', async () => {
+    const { response } = await getAllFromContacts()
+    expect(response.body).toHaveLength(initialContacts.length)
+  })
 
-  const { names } = await getAllFromContacts()
-  expect(names).toContain('Carlos Lozano')
-  expect(names).toHaveLength(initialContacts.length + 1)
-})
-
-test('a invalid contact cannot be added', async () => {
-  const newBadConctact = {
-    name: 'bad user',
-    phone: undefined
-  }
-
-  await api
-    .post('/api/persons')
-    .send(newBadConctact)
-    .expect(400)
-
-  const { names } = await getAllFromContacts()
-  expect(names).toHaveLength(initialContacts.length)
-})
-
-test('a invalid id return 404 status code', async () => {
-  const invalidID = '61f85179ff08013914dd882d'
-
-  await api
-    .get(`/api/persons/${invalidID}`)
-    .expect(404)
-})
-
-test('a contact can be deleted', async () => {
-  const { response: firstResponse } = await getAllFromContacts()
-  const contacts = firstResponse.body
-  const firstContact = contacts[0]
-
-  await api
-    .delete(`/api/persons/${firstContact.id}`)
-    .expect(204)
-
-  const { names, response: secondResponse } = await getAllFromContacts()
-  expect(secondResponse.body).toHaveLength(initialContacts.length - 1)
-
-  expect(names).not.toContain(firstContact.name)
-})
-
-test('a contact that not exist cannot be deleted', async () => {
-  const invalidPATH = '/api/persons/1234'
-
-  await api
-    .delete(invalidPATH)
-    .expect(400)
-
-  const { response } = await getAllFromContacts()
-  expect(response.body).toHaveLength(initialContacts.length)
+  test('a contact have my name', async () => {
+    const { names } = await getAllFromContacts()
+    expect(names).toContain('Agustin Lozano')
+  })
 })
 
 afterAll(() => {
