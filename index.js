f3408187dac67283e315740a853f53bb177c14b1
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2022-02-14 20:32:33 -0300

    Edit index

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const logger = require('./utils/logger')
 
 const server = http.createServer(app)
 
-server.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`)
+const PORT = config.PORT || 3001
+server.listen(PORT, () => {
+  logger.info(`Server running on port ${PORT}`)
 })
 
 module.exports = server
